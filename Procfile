d643c2087df95f7ccc6158b08053e1ad308c7f09
drinkcoffee <dcoffie92@gmail.com>
2019-11-20 19:52:02 -0800

    nightmare heroku

---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: DEBUG=* xvfb-run --auto-servernum --server-args="-screen 0 1024x768x24" node --harmony index.js
+
+web: node server.js
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 //imports .env variables
-require('dotenv').config()
+require('dotenv').config({path: './backend/.env'})
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -19,8 +19,8 @@ connection.once('open', () => {
     console.log('MongoDB database connection established successfully')
 }).then(() => { }).catch(err => console.error(err))
 
-const moviesRouter = require('./routes/movieAPI')
-const resturantsRouter = require('./routes/resturantAPI')
+const moviesRouter = require('./backend/routes/movieAPI')
+const resturantsRouter = require('./backend/routes/resturantAPI')
 
 app.use('/movies', moviesRouter)
 app.use('/resturants', resturantsRouter)
